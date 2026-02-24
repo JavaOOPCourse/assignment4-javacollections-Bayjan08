87f83fb6a82984d513a242d9014853bfbc3a8c8e
Bayjan08 <125944089+Bayjan08@users.noreply.github.com>
2026-02-24 12:47:28 +0600

    services

--- src/service/ActionHistoryService.java
+++ src/service/ActionHistoryService.java
@@ -1,5 +1,6 @@
 package service;
 
+import javax.crypto.spec.PSource;
 import java.util.ArrayDeque;
 import java.util.Iterator;
 
@@ -8,22 +9,47 @@ public class ActionHistoryService {
     private ArrayDeque<String> actions = new ArrayDeque<>();
 
     public void initializeActions() {
-        // TODO: Add 4 actions
+        actions.addLast("Submitted Assignment");
+        actions.addLast("Dropped Course");
+        actions.addLast("Registered Course");
+        actions.addLast("Updated Profile");
     }
 
     public void undoLastAction() {
-        // TODO: Remove last action
+       if (actions.isEmpty()){
+           System.out.println("History is empty!!");
+           return;
+       }
+       String removed = actions.removeLast();
+       System.out.println("Undone: " + removed);
     }
 
     public void addRequestedTranscript() {
         // TODO: Add "Requested Transcript"
+        actions.addLast("Requested Transcript");
+        System.out.println("Added: Requested Transcript");
+
     }
 
     public void showFirstAndLast() {
-        // TODO: Print first and last action
+        if (actions.isEmpty()) {
+            System.out.println("History is empty.");
+            return;
+        }
+        System.out.println("First action: " + actions.peekFirst());
+        System.out.println("Last action: " + actions.peekLast());
     }
 
     public void printHistory() {
         // TODO: Iterate through history
+        if (actions.isEmpty()) {
+            System.out.println("History is empty.");
+            return;
+        }
+        System.out.println("=== Action History ===");
+        Iterator<String> it = actions.iterator();
+        while (it.hasNext()) {
+            System.out.println("- " + it.next());
+        }
     }
 }
--- src/service/AppointmentService.java
+++ src/service/AppointmentService.java
@@ -1,5 +1,6 @@
 package service;
 
+import javax.crypto.spec.PSource;
 import java.util.LinkedList;
 import java.util.Iterator;
 
@@ -8,19 +9,44 @@ public class AppointmentService {
     private LinkedList<String> appointments = new LinkedList<>();
 
     public void initializeAppointments() {
-        // TODO: Add 3 normal appointments
         // TODO: Add 1 urgent appointment at the beginning
+        appointments.addFirst("URGENT: Meet Dean (Academic Warning)");
+
+        // TODO: Add 3 normal appointments
+        appointments.addLast("Advisor Meeting: Course Plan");
+        appointments.addLast("Finance Office: Scholarship Info");
+        appointments.addLast("Library: Book Renewal Help");
     }
 
     public void cancelLast() {
         // TODO: Remove last appointment
+        if (appointments.isEmpty()) {
+            System.out.println("No appointments to cancel.");
+            return;
+        }
+        String removed = appointments.removeLast();
+        System.out.println("Canceled: " + removed);
     }
 
     public void showFirstAndLast() {
         // TODO: Print first and last appointment
+        if (appointments.isEmpty()) {
+            System.out.println("No appointments.");
+            return;
+        }
+        System.out.println("First appointment: " + appointments.getFirst());
+        System.out.println("Last appointment: " + appointments.getLast());
     }
 
     public void printAppointments() {
-        // TODO: Traverse using Iterator
+        if (appointments.isEmpty()) {
+            System.out.println("No appointments.");
+            return;
+        }
+        System.out.println("=== Appointments ===");
+        Iterator<String> it = appointments.iterator();
+        while (it.hasNext()) {
+            System.out.println("- " + it.next());
+        }
     }
 }
