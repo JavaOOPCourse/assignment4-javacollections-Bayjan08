b7c3b562b5d534288e1ce2817ce6693d35aa8974
Bayjan08 <125944089+Bayjan08@users.noreply.github.com>
2026-02-24 12:46:48 +0600

    models

--- src/model/Student.java
+++ src/model/Student.java
@@ -8,9 +8,21 @@ public class Student {
     private double gpa;
 
     // TODO: create constructor
+    public Student(){}
+    public Student(int id, String name, double gpa){
+        this.id = id;
+        this.name = name;
+        this.gpa = gpa;
+    }
 
     // TODO: create getters
+    public double getGpa() { return gpa; }
+    public String getName() { return name; }
+    public int getId() { return id; }
 
     // TODO: override toString()
+    public String toString(){
+        return "Name: " + getName() + " || " + "Gpa: " + getGpa();
+    }
 
 }
